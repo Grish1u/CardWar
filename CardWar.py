28646202c34ce3ae4c3e82823e34a02edafb4774
gri <grish@gmail.com>
2019-12-27 11:54:21 +0200

    Class methods table.turn(), table.war() and table.winnerSituation() added, also method player.shuffleTaken() added

--- CardWar.py
+++ CardWar.py
@@ -71,11 +71,16 @@ class Player(object):
 	def receiveCard(self, card):
 		self.taken.append(card)
 
+	# second time I have to make shuffle method - add this in todo list. To make only 1 shuffle methiods for all lists of card.
+	# might have to change the player card and taken lists to some kind of a deck object in order to do the Todo
+	def shuffleTaken(self):
+		print('SHUFFLING ' + self.name + "'s cards")
+		random.shuffle(self.taken)
+	
 	def takenToHand(self):
 		print("TAKEN TO HAND CONVERTION")
-		# for c in self.taken:
-		# 	self.hand.append(c)
-		# 	self.taken.remove(self.taken[0])
+
+		self.shuffleTaken()
 		for card in range(1, len(self.taken) + 1):
 			self.hand.append(self.taken[0])
 			self.taken.remove(self.taken[0])
@@ -93,7 +98,16 @@ class Player(object):
 			c.printCard()
 		if len(self.taken) == 0:
 			print("No cards in Taken")
+	
+	def printNumCards(self):
+		total = len(self.hand) + len(self.taken)
+		print(self.name + ' has ' + str(total) + ' cards')
+
+	def gainAWin(self):
+		self.wins += 1
+
 	def scores(self):
+		# maybe some more robust type of scoring pane
 		return wins
 
 class Table(object):
@@ -103,25 +117,54 @@ class Table(object):
 		self.p2 = player2
 		self.playZone = []
 	
-	def razdavane(self):
+	# This method gives 26 cards yeach player 1 by 1
+	# The Deck will have 0 elements after this operation
+	def deckDeals(self):
 		#self.deck.shuffleDeck()
-		for i in range(1, self.deck.getSize()):
+		while deck.getSize() > 0:
 			
 			self.p1.receiveCard(deck.getTopCard())
 			self.deck.removeTopCard()
 			
 			self.p2.receiveCard(deck.getTopCard())
 			self.deck.removeTopCard()
+		
 		print('deck lenght: ' + str(deck.getSize()))
 		print(self.p1.printTaken())
 		print(self.p2.printTaken())
 
+	# This method will reward the player with stronger card
+	# with the cards in the playzone
+	def winnerSituation(player):
+		# rewards a player a list of cards
+		# deletes these cards from the playzone
+		for card in self.playZone:
+			player.receiveCard(card)	
+		
+		self.playZone.clear() # equivalent to self.playZone = [] or del self.playzone[:]
 
-	# def turn(self, p1, p2):
-	# 	card = p1.giveCard()
-	# 	card2 = p2.giveCard()
-	# 	playZone.append(card)
-	# 	playZone.append(card2)
+
+
+
+	def turn(self, p1, p2):
+	 	
+	 	card = p1.giveCard()
+	 	card2 = p2.giveCard()
+	 	playZone.append(card)
+	 	playZone.append(card2)
+	 	
+	 	if card.compareWith(card2) == True:
+			winnerSituation(p1)
+		
+		elif card.compareWith(card2) == False:
+			winnerSituation(p2)
+		
+		else: # when its neither True nor False then they are the same
+			war()
+	
+	def war(self, p1, p2):
+
+
 
 		
 
@@ -129,12 +172,23 @@ class Table(object):
 
 
 while True:
-
+	# init the two players
     grigor = Player('grigor')
     teodor = Player('teodor')
+
+    # init the deck
     deck = Deck()
+
+    # init the table
     table = Table(deck, grigor, teodor)
-    table.razdavane()
+    
+    # the cards from deck to players
+    table.deckDeals()
+    # printout to check for errors
+    grigor.printNumCards()
+    teodor.printNumCards()
+
+
 
 
     
